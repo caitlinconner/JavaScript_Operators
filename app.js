241c8ddd9abdccf0708e030289433f55c288b8e7
Caitlin Conner <caitlinjconner@gmail.com>
2023-04-15 18:50:45 -0500

    Operators Exercise

--- app.js
+++ app.js
@@ -6,14 +6,44 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+// 1. 1111
+// 2. 11eleven
+// 3. 12
+// 4. Frodo11
+// 5. true11
+// 6. Frodo Baggins
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+// 1. True
+// 2. False
+// 3. True
+// 4. False
+// 5. True
+// 6. True
+// 7. False
+// 8. False
+// 9. True 
